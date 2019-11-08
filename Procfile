0979ea7383afacacb9825e716c67de272e33929d
Deepak Rajpurohit <princerajpurohit945@gmail.com>
2019-11-08 18:28:31 +0530

    First Commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn gettingstarted.wsgi
+web: gunicorn app:app --log-file=-
---
+++ app/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY ='a34d0c212fe137ab1e1b93864a673a7a'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'princerajpurohit945@gmail.com'
+    MAIL_PASSWORD = 'deepak121rajpurohit'
